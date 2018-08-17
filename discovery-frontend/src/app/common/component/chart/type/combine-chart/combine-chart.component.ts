aa25ef56918fd100ab4b8810c66f3185245e8cee
Taeho Lee <exntu-metatron@googlegroups.com>
2018-08-17 17:45:36 +0900

    #2 y axis min/max input bug

--- discovery-frontend/src/app/common/component/chart/type/combine-chart/combine-chart.component.ts
+++ discovery-frontend/src/app/common/component/chart/type/combine-chart/combine-chart.component.ts
@@ -241,7 +241,7 @@ export class CombineChartComponent extends BaseChart implements OnInit, OnDestro
 
         // 기준선 변경시
         let baseline = 0;
-        if( axisOption[index].baseline != 0 ) {
+        if( axisOption[index].baseline && axisOption[index].baseline != 0 ) {
           baseline = axisOption[index].baseline
         }
 
