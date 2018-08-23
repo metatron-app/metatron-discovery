8e90ba2e77cb64f8bba3af2efc9a3f513821e9df
Daeen Choi <paigechoi@exntu.com>
2018-08-23 17:25:45 +0900

    #33 show snapshot status in edit-rule snapshot tab

--- discovery-frontend/src/app/data-preparation/dataflow/dataflow-detail/component/edit-dataflow-rule/rule-list.component.ts
+++ discovery-frontend/src/app/data-preparation/dataflow/dataflow-detail/component/edit-dataflow-rule/rule-list.component.ts
@@ -122,6 +122,7 @@ export class RuleListComponent extends AbstractComponent implements OnInit, OnDe
    * @param dsId
    */
   public getSnapshotWithInterval(dsId) {
+    clearInterval(this.interval);
     this.interval = setInterval(() => {
       this.getSnapshotList(dsId);
     },1000)
@@ -133,17 +134,20 @@ export class RuleListComponent extends AbstractComponent implements OnInit, OnDe
    * @return {number} interval을 stop 하려면 -1을 리턴한다.
    */
   public pollingContinue(list) : number {
+    let statusList = ['INITIALIZING','RUNNING','WRITING','TABLE_CREATING','NOT_AVAILABLE'];
     return list.findIndex((item) => {
-      return !item.elapsedTime
+      return -1 !== statusList.indexOf(item.status)
     });
   } // function - pollingContinue
 
   /**
    * 스냅샷 리스트 호출
    * @param {string} dsId
+   * @param {boolean} withAll
    */
   public getSnapshotList(dsId : string) {
-    this.dataflowService.getWorkList(dsId).then((result) => {
+    let params = {dsId : dsId};
+    this.dataflowService.getWorkList(params).then((result) => {
       if(result['snapshots'] && 0 < result['snapshots'].length ) {
         this.snapshotList = result['snapshots'];
         if (!isUndefined(this.ruleSnapshotListComponent)) { // 현재 탭이 snapshot list 아닐 때 을 하면 에러..
@@ -191,6 +195,13 @@ export class RuleListComponent extends AbstractComponent implements OnInit, OnDe
     this.snapshotDetailEvent.emit(data);
   } // function - snapshotDetail
 
+  public setCanceledStatus(ssId) {
+    this.snapshotList.forEach((item) => {
+      if (item.ssId === ssId) {
+          item.status = 'Canceled';
+      }
+    })
+  }
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   | Protected Method
   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
