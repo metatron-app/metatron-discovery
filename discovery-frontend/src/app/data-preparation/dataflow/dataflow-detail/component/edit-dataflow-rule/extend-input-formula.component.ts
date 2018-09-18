1279f3240d058a7dcf49b686247bcca57271e4a2
garlic <garlic@exntu.com>
2018-09-18 11:24:06 +0900

    #188 Add more examples and explanations to expressions in advanced editor

--- discovery-frontend/src/app/data-preparation/dataflow/dataflow-detail/component/edit-dataflow-rule/extend-input-formula.component.ts
+++ discovery-frontend/src/app/data-preparation/dataflow/dataflow-detail/component/edit-dataflow-rule/extend-input-formula.component.ts
@@ -24,6 +24,7 @@ import { AbstractComponent } from '../../../../../common/component/abstract.comp
 import { Field } from '../../../../../domain/data-preparation/dataset';
 import { StringUtil } from '../../../../../common/util/string.util';
 import { DataflowService } from '../../../service/dataflow.service';
+import { Alert } from '../../../../../common/util/alert.util';
 import * as _ from 'lodash';
 declare let $;
 
@@ -113,15 +114,18 @@ export class ExtendInputFormulaComponent extends AbstractComponent implements On
    * @param {Field[]} fields
    * @param {string} command : 수식 입력 대상 커맨드 명
    */
-  public open(fields: Field[], command: string) {
+  public open(fields: Field[], command: string, forceCondition? : string ) {
     this.isShow = true;
     this._command = command;
+
+    if ( (! forceCondition) || forceCondition == null )  forceCondition = '';
 
     // 필드 설정
     this._fields = _.cloneDeep(fields);
 
     // set 은 field 목록에 $col 추가
     if ('set' === this._command) {
+      //this._fields.unshift({name :'$col', type : 'STRING'});
       this._fields.unshift({name :'$col', type : 'STRING'});
     }
 
@@ -130,6 +134,8 @@ export class ExtendInputFormulaComponent extends AbstractComponent implements On
     // Input 영역 설정
     this.safelyDetectChanges();
     this._$calculationInput = $(this._calculationInput.nativeElement);
+
+    this._$calculationInput.text(forceCondition);
 
     // 자동완성 설정
     this._setAutoComplete();
@@ -146,20 +152,31 @@ export class ExtendInputFormulaComponent extends AbstractComponent implements On
    * 수식 적용 및 팝업 닫기
    */
   public done() {
-
     // verifyStateFormula 가 Success 아닐때
-    if ('S' !== this.verifyStateFormula) {
-      return;
-    }
+    // if ('S' !== this.verifyStateFormula) {
+    //  return;
+    // }
 
     let expr: string = this._$calculationInput.text();
+
     // expr = expr.replace(/[[\]]/g, '"');
     expr = StringUtil.trim(expr);
+    if (! expr || expr.length < 1){
+      Alert.info(this.translateService.instant('msg.dp.alert.check.insert.expression'));
+      return;
+    }
 
     // 수식 반환
     this.doneEvent.emit({ command: this._command, formula: expr });
     this.close();
   } // function - done
+  /**
+   * 수식 입력 영역 키다운 이벤트 함수
+   */
+  public calculationInputKeyup(){
+      this.verifyStateFormula = null;
+      this._setDisableVerifyButton();``
+  }
 
   /**
    * 수식 검증
@@ -396,7 +413,7 @@ export class ExtendInputFormulaComponent extends AbstractComponent implements On
   } // function - _insertAtCursor
 
   /**
-   * 수식 검증 버튼 활성화 여부
+   * 수식 검증 버튼 활성화 여부 -> 조건 없이 활성화
    */
   private _setDisableVerifyButton() {
     this.verifyStateFormula = null;
@@ -411,141 +428,141 @@ export class ExtendInputFormulaComponent extends AbstractComponent implements On
 
     // 함수 목록 설정
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'length', '입력된 문자열의 길이를 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'length', 'msg.dp.ui.expression.functiondesc.string.length', 'length(‘hello world’)<br>&gt;11')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'upper', '입력된 문자열 내의 알파벳을 모두 대문자로 치환하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'upper', 'msg.dp.ui.expression.functiondesc.string.upper', 'upper(‘Hello world’)<br>&gt;’HELLO WORLD’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'lower', '입력된 문자열 내의 알파벳을 모두 소문자로 치환하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'lower', 'msg.dp.ui.expression.functiondesc.string.lower', 'lower(‘Hello WORLD’)<br>&gt;’hello world’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'trim', '입력된 문자열의 앞/뒤에 있는 공백을 제거하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'trim', 'msg.dp.ui.expression.functiondesc.string.trim','trim(‘  .   Hi!   ‘)<br>&gt;‘.   Hi!’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'ltrim', '입력된 문자열의 앞(왼쪽)에 있는 공백을 제거하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'ltrim', 'msg.dp.ui.expression.functiondesc.string.ltrim','ltrim(‘  .   Hi!   ‘)<br>&gt;’.   Hi!   ‘')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'rtrim', '입력된 문자열의 뒤(오른쪽)에 있는 공백을 제거하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'rtrim', 'msg.dp.ui.expression.functiondesc.string.rtrim','rtrim(‘  .   Hi!   ‘)<br>&gt;‘  .   Hi!’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'substring', '입력된 문자열의 일부를 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'substring', 'msg.dp.ui.expression.functiondesc.string.substring.','substring(‘hello world’, 1, 7)<br>&gt;‘ello w’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'concat', '입력된 복수의 문자열을 연결하여 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'concat', 'msg.dp.ui.expression.functiondesc.string.concat','concat(‘1980’, ’02’)<br>&gt;‘198002’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.STRING, 'concat_ws', '입력된 복수의 문자열을 연결하면서 문자열 사이에 Separator(구분자)를 넣어 반환합니다.')
+      new FormulaFunction(FunctionCategory.STRING, 'concat_ws', 'msg.dp.ui.expression.functiondesc.string.concat_ws','concat(separator, stirng_value1, string_value2)<br>concat_ws(‘-‘, ‘010’, ‘1234’, ‘5678’)<br>&gt;’010-1234-5678’')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.LOGICAL, 'if', '조건문을 검사하여 TRUE나 FALSE에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.LOGICAL, 'if', 'msg.dp.ui.expression.functiondesc.logical.if','if(gender==‘male’)<br>&gt;TRUE')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.LOGICAL, 'ismismatched', '입력된 컬럼의 값과 타입이 일치하는지 판단합니다. 일치하면 TRUE, 아니면 FALSE를 반환합니다.')
+      new FormulaFunction(FunctionCategory.LOGICAL, 'ismismatched', 'msg.dp.ui.expression.functiondesc.logical.ismismatched','')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.LOGICAL, 'isnull', '입력된 컬럼의 값이 null 인지 판단합니다. null이면 TRUE, 아니면 FALSE를 반환합니다.')
+      new FormulaFunction(FunctionCategory.LOGICAL, 'isnull', 'msg.dp.ui.expression.functiondesc.logical.isnull','isnull(telephone)<br>&gt;FALSE')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.LOGICAL, 'isnan', '입력된 값이 NaN(Not-a-Number) 인지 판단합니다. NaN이면 TRUE, 아니면 FALSE를 반환합니다.')
+      new FormulaFunction(FunctionCategory.LOGICAL, 'isnan', 'msg.dp.ui.expression.functiondesc.logical.isnan','isnan(1000/ratio)<br>&gt;FALSE')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'year', '입력된 Timestamp 값에서 연도에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'year', 'msg.dp.ui.expression.functiondesc.timestamp.year','year(birthday)<br>&gt; 1987')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'month', '입력된 Timestamp 값에서 월에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'month', 'msg.dp.ui.expression.functiondesc.timestamp.month','month(birthday)<br>&gt; 2')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'day', '입력된 Timestamp 값에서 일에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'day', 'msg.dp.ui.expression.functiondesc.timestamp.day','day(birthday)<br>&gt; 13')
+  );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'hour', 'msg.dp.ui.expression.functiondesc.timestamp.hour','hour(last_login)<br>&gt; 21')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'hour', '입력된 Timestamp 값에서 시간에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'minute', 'msg.dp.ui.expression.functiondesc.timestamp.minute','minute(last_login)<br>&gt; 49')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'minute', '입력된 Timestamp 값에서 분에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'second', 'msg.dp.ui.expression.functiondesc.timestamp.second','second(last_login)<br>&gt; 28')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'second', '입력된 Timestamp 값에서 초에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'millisecond', 'msg.dp.ui.expression.functiondesc.timestamp.millisecond','millisecond(last_login)<br>&gt; 831')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'millisecond', '입력된 Timestamp 값에서 밀리초(1/1000 초)에 해당하는 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'now', 'msg.dp.ui.expression.functiondesc.timestamp.now','now()<br>&gt;2018-04-18T12:20:90.220Z')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'now', '입력된 Timezone 기준의 현재 시간을 반환합니다.')
+      new FormulaFunction(FunctionCategory.TIMESTAMP, 'add_time', 'msg.dp.ui.expression.functiondesc.timestamp.add_time','add_time(timestamp, delta, time_unit)<br>&gt;add_time(end_date, 10, ‘day’)')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.TIMESTAMP, 'add_time', '입력된 Timestamp 값에 일정 Time unit 값을 더하거나 뺀 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.AGGREGATION, 'sum', 'msg.dp.ui.expression.functiondesc.aggregation.sum','sum(profit)')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.AGGREGATION, 'sum', '대상 값들의 합을 반환합니다.')
+      new FormulaFunction(FunctionCategory.AGGREGATION, 'avg', 'msg.dp.ui.expression.functiondesc.aggregation.avg','avg(profit)')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.AGGREGATION, 'avg', '대상 값들의 평균을 반환합니다.')
+      new FormulaFunction(FunctionCategory.AGGREGATION, 'max', 'msg.dp.ui.expression.functiondesc.aggregation.max','max(profit)')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.AGGREGATION, 'max', '대상 값들 중 가장 큰 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.AGGREGATION, 'min', 'msg.dp.ui.expression.functiondesc.aggregation.min','min(profit)')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.AGGREGATION, 'min', '대상 값들 중 가장 작은 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.AGGREGATION, 'count', 'msg.dp.ui.expression.functiondesc.aggregation.count','count()')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.AGGREGATION, 'count', '대상의 줄(row)수를 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.abs', 'msg.dp.ui.expression.functiondesc.math.abs','math.abs(-10)<br>&gt;10')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.abs', '입력된 값의 절대값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.acos', 'msg.dp.ui.expression.functiondesc.math.acos','math.acos(-1)<br>&gt; 3.141592653589793')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.acos', '입력된 값의 아크코사인 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.asin', 'msg.dp.ui.expression.functiondesc.math.asin','math.asin(-1)<br>&gt;-1.5707963267948966')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.asin', '입력된 값의 아크사인 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.atan', 'msg.dp.ui.expression.functiondesc.math.atan','math.atan(-1)<br>&gt;-0.7853981633974483')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.atan', '입력된 값의 아크탄젠트 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.cbrt', 'msg.dp.ui.expression.functiondesc.math.cbrt','math.cbrt(5)<br>&gt; 1.709975946676697')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.cbrt', '입력된 값의 세제곱근 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.ceil', 'msg.dp.ui.expression.functiondesc.math.ceil','math.ceil(15.142)<br>&gt; 16')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.ceil', '입력된 값을 일의 배수가 되도록 올림한 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.cos', 'msg.dp.ui.expression.functiondesc.math.cos','math.cos(45)<br>&gt;0.5253219888177297')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.cos', '입력된 값의 코사인 값을 반환합니다.')
+      new FormulaFunction(FunctionCategory.MATH, 'math.cosh', 'msg.dp.ui.expression.functiondesc.math.cosh','math.cosh(9)<br>&gt;COSH(9) &equals;&gt; 4051.5420254925943')
     );
     this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.cosh', '입력된 값의 하이퍼볼릭 코사인 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.exp', '자연 로그값 e를 입력된 값만큼 거듭제곱한 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.expm1', '자연 로그값 e를 입력된 값만큼 거듭제곱한 값에서 1을 뺀 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.getExponent', '입력된 값 N에 대하여 2exp <= N을 만족하는 exp 값 중 가장 큰 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.round', '입력된 값을 일의 자리로 반올림 한 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.signum', '입력된 값의 부호를 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.sin', '입력된 값의 사인 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.sinh', '입력된 값의 하이퍼볼릭 사인 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.sqrt', '입력된 값의 제곱근을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.tan', '입력된 값의 탄젠트 값을 반환합니다.')
-    );
-    this._functionList.push(
-      new FormulaFunction(FunctionCategory.MATH, 'math.tanh', '입력된 값의 하이퍼볼릭 탄젠트 값을 반환합니다.')
-    );
+      new FormulaFunction(FunctionCategory.MATH, 'math.exp', 'msg.dp.ui.expression.functiondesc.math.exp','math.exp(4)<br>&gt;54.598150033144236')
 
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.expm1', 'msg.dp.ui.expression.functiondesc.math.expm1','math.expm1(4)<br>&gt;53.598150033144236')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.getExponent', 'msg.dp.ui.expression.functiondesc.math.getExponent','math.getExponent(9)<br>&gt;3')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.round', 'msg.dp.ui.expression.functiondesc.math.round','math.round(14.2)<br>&gt;14')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.signum', 'msg.dp.ui.expression.functiondesc.math.signum','math.signum(-24)<br>&gt;-1')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.sin', 'msg.dp.ui.expression.functiondesc.math.sin','math.sin(90)<br>&gt;0.8939966636005579')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.sinh', 'msg.dp.ui.expression.functiondesc.math.sinh','math.sinh(1)<br>&gt;1.1752011936438014')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.sqrt', 'msg.dp.ui.expression.functiondesc.math.sqrt','math.sqrt(4)<br>&gt;2')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.tan', 'msg.dp.ui.expression.functiondesc.math.tan','math.tan(10)<br>&gt;0.6483608274590866')
+    );
+    this._functionList.push(
+      new FormulaFunction(FunctionCategory.MATH, 'math.tanh', 'msg.dp.ui.expression.functiondesc.math.tanh','math.tanh(4)<br>&gt;0.999329299739067')
+    );
 
     // 카테고리 목록 설정
     this.totalFunctionCategoryList.push(
@@ -619,9 +636,10 @@ class FormulaFunction {
   public description: string;
   public example: string;
 
-  constructor(category: FunctionCategory, name: string, description: string) {
+  constructor(category: FunctionCategory, name: string, description: string, example: string) {
     this.category = category;
     this.name = name;
     this.description = description;
+    this.example = example;
   }
 }
