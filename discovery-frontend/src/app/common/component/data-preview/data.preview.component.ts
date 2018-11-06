c12eb4071638703203942739227b9572e37f489f
Junghwan Park <reserve.dev@gmail.com>
2018-11-06 17:50:24 +0900

    #363 fix quartile values in data preview

--- discovery-frontend/src/app/common/component/data-preview/data.preview.component.ts
+++ discovery-frontend/src/app/common/component/data-preview/data.preview.component.ts
@@ -620,10 +620,10 @@ export class DataPreviewComponent extends AbstractPopupComponent implements OnIn
     if (stats.hasOwnProperty('iqr')) {
       const length = stats.iqr.length;
       switch (type) {
-        case 'UPPER':
+        case 'LOWER':
           result = stats.iqr[0];
           break;
-        case 'LOWER':
+        case 'UPPER':
           result = stats.iqr[length - 1];
           break;
       }
