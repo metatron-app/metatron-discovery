1383a87a655ac59726ffdef9f25ff4e58d1c2107
juheeko <exntu-metatron@googlegroups.com>
2018-11-06 15:49:07 +0900

    #659 editing map common, layer panel

--- discovery-frontend/src/app/page/chart-style/map/map-layer-option.component.ts
+++ discovery-frontend/src/app/page/chart-style/map/map-layer-option.component.ts
@@ -15,6 +15,11 @@ import { Component, ElementRef, Injector, Input } from '@angular/core';
 import { BaseOptionComponent } from '../base-option.component';
 import { Pivot } from '../../../domain/workbook/configurations/pivot';
 import { UIMapOption } from '../../../common/component/chart/option/ui-option/map/ui-map-chart';
+import { MapLayerType, MapSymbolType } from '../../../common/component/chart/option/define/map/map-common';
+import * as _ from 'lodash';
+import { UIOption } from '../../../common/component/chart/option/ui-option';
+import { SymbolType } from '../../../common/component/chart/option/define/common';
+import { UISymbolLayer } from '../../../common/component/chart/option/ui-option/map/ui-symbol-layer';
 
 @Component({
   selector: 'map-layer-option',
@@ -22,16 +27,76 @@ import { UIMapOption } from '../../../common/component/chart/option/ui-option/ma
 })
 export class MapLayerOptionComponent extends BaseOptionComponent {
 
-  @Input('uiOption')
+  // current layer index (0-2)
+  @Input('index')
+  public index: number;
+
   public uiOption: UIMapOption;
+
+  @Input('uiOption')
+  public set setUiOption(uiOption: UIMapOption) {
+
+    this.uiOption = uiOption;
+  }
 
   // 선반데이터
   @Input('pivot')
   public pivot: Pivot;
 
+  // symbol layer - type list
+  public symbolLayerTypes = [{name : this.translateService.instant('msg.page.layer.map.type.point'), value: MapLayerType.SYMBOL},
+                             {name : this.translateService.instant('msg.page.layer.map.type.heatmap'), value: MapLayerType.HEATMAP},
+                             {name : this.translateService.instant('msg.page.layer.map.type.tile'), value: MapLayerType.TILE}];
+
+  // symbol layer - symbol list
+  public symbolLayerSymbols = [{name : this.translateService.instant('msg.page.layer.map.point.circle'), value : MapSymbolType.CIRCLE},
+                               {name : this.translateService.instant('msg.page.layer.map.point.square'), value : MapSymbolType.SQUARE},
+                               {name : this.translateService.instant('msg.page.layer.map.point.triangle'), value : MapSymbolType.TRIANGLE},
+                               {name : this.translateService.instant('msg.page.layer.map.point.pin'), value : MapSymbolType.PIN},
+                               {name : this.translateService.instant('msg.page.layer.map.point.plain'), value : MapSymbolType.PLAIN},
+                               {name : this.translateService.instant('msg.page.layer.map.point.people'), value : MapSymbolType.USER}];
+
   constructor(protected elementRef: ElementRef,
               protected injector: Injector) {
 
     super(elementRef, injector);
+  }
+
+  /**
+   * symbol layer - change layer type
+   * @param {MapLayerType} layerType
+   */
+  public changeSymbolLayerType(layerType : MapLayerType) {
+
+    this.uiOption.layers[this.index].type = layerType;
+
+    // apply layer ui option
+    this.applyLayers();
+  }
+
+  /**
+   * symbol layer - change symbol type
+   * @param {SymbolType} symbolType
+   */
+  public changeSymbolType(symbolType: MapSymbolType) {
+
+    (<UISymbolLayer>this.uiOption.layers[this.index]).symbol = symbolType;
+
+    // apply layer ui option
+    this.applyLayers();
+  }
+
+
+
+  /**
+   * apply layer ui option
+   */
+  private applyLayers() {
+
+    this.uiOption = <UIMapOption>_.extend({}, this.uiOption, {
+      layers: this.uiOption.layers
+    });
+
+    this.update();
   }
 }
