9f85d52f322bd7a84fd42d60d7810a2cb4a34425
changwoo sohn <ufoscw@gmail.com>
2018-11-20 10:14:32 +0900

    #684 add datasource list filter api

--- discovery-server/src/main/java/app/metatron/discovery/domain/datasource/DataSourcePredicate.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/datasource/DataSourcePredicate.java
@@ -14,16 +14,16 @@
 
 package app.metatron.discovery.domain.datasource;
 
+import app.metatron.discovery.domain.workspace.Workspace;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
-
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 
-import app.metatron.discovery.domain.workspace.Workspace;
+import java.util.List;
 
 public class DataSourcePredicate {
 
@@ -135,6 +135,96 @@ public class DataSourcePredicate {
     // Datasource 명 검색
     if (StringUtils.isNotEmpty(nameContains)) {
       builder.and(dataSource.name.containsIgnoreCase(nameContains));
+    }
+
+    return builder;
+  }
+
+  public static Predicate searchList(List<DataSource.Status> statuses,
+                                     List<String> workspaces,
+                                     List<String> createdBys,
+                                     DateTime createdTimeFrom,
+                                     DateTime createdTimeTo,
+                                     DateTime modifiedTimeFrom,
+                                     DateTime modifiedTimeTo,
+                                     String containsText,
+                                     List<DataSource.DataSourceType> dataSourceTypes,
+                                     List<DataSource.SourceType> sourceTypes,
+                                     List<DataSource.ConnectionType> connectionTypes){
+    BooleanBuilder builder = new BooleanBuilder();
+    QDataSource dataSource = QDataSource.dataSource;
+
+    //Status
+    if(statuses != null && !statuses.isEmpty()){
+      BooleanBuilder subBuilder = new BooleanBuilder();
+      for(DataSource.Status status : statuses){
+        subBuilder = subBuilder.or(dataSource.status.eq(status));
+      }
+      builder = builder.and(subBuilder);
+    }
+
+    //DataSourceType
+    if(dataSourceTypes != null && !dataSourceTypes.isEmpty()){
+      BooleanBuilder subBuilder = new BooleanBuilder();
+      for(DataSource.DataSourceType dataSourceType : dataSourceTypes){
+        subBuilder = subBuilder.or(dataSource.dsType.eq(dataSourceType));
+      }
+      builder = builder.and(subBuilder);
+    } else {
+      builder = builder.and(dataSource.dsType.ne(DataSource.DataSourceType.VOLATILITY));
+    }
+
+    //SourceType
+    if(sourceTypes != null && !sourceTypes.isEmpty()){
+      BooleanBuilder subBuilder = new BooleanBuilder();
+      for(DataSource.SourceType sourceType : sourceTypes){
+        subBuilder = subBuilder.or(dataSource.srcType.eq(sourceType));
+      }
+      builder = builder.and(subBuilder);
+    }
+
+    //ConnectionType
+    if(connectionTypes != null && !connectionTypes.isEmpty()){
+      BooleanBuilder subBuilder = new BooleanBuilder();
+      for(DataSource.ConnectionType connectionType : connectionTypes){
+        subBuilder = subBuilder.or(dataSource.connType.eq(connectionType));
+      }
+      builder = builder.and(subBuilder);
+    }
+
+    //CreatedBy
+    if(createdBys != null && !createdBys.isEmpty()){
+      BooleanBuilder subBuilder = new BooleanBuilder();
+      for(String createdBy : createdBys){
+        subBuilder = subBuilder.or(dataSource.createdBy.eq(createdBy));
+      }
+      builder = builder.and(subBuilder);
+    }
+
+    //containsText
+    if(StringUtils.isNotEmpty(containsText)){
+      builder = builder.andAnyOf(dataSource.name.containsIgnoreCase(containsText),
+              dataSource.description.containsIgnoreCase(containsText));
+    }
+
+    //createdTime
+    if(createdTimeFrom != null && createdTimeTo != null) {
+      builder = builder.and(dataSource.createdTime.between(createdTimeFrom, createdTimeTo));
+    }
+
+    //modifiedTime
+    if(modifiedTimeFrom != null && modifiedTimeTo != null) {
+      builder = builder.and(dataSource.modifiedTime.between(modifiedTimeFrom, modifiedTimeTo));
+    }
+
+    //workspaces
+    if(workspaces != null && !workspaces.isEmpty()){
+      BooleanExpression workspaceContains = dataSource.id
+              .in(JPAExpressions.select(dataSource.id)
+                      .from(dataSource)
+                      .innerJoin(dataSource.workspaces)
+                      .where(dataSource.workspaces.any().id.in(workspaces)));
+      builder.and(workspaceContains);
     }
 
     return builder;
