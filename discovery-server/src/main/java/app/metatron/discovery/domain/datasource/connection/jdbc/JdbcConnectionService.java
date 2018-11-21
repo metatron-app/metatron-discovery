651f3f6bdd75c504e0a1ef8ec489dacad55022b7
Kyungtaak Noh <kyungtaak@gmail.com>
2018-11-22 07:37:08 +0900

    #728 fix merge error

--- discovery-server/src/main/java/app/metatron/discovery/domain/datasource/connection/jdbc/JdbcConnectionService.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/datasource/connection/jdbc/JdbcConnectionService.java
@@ -66,6 +66,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
