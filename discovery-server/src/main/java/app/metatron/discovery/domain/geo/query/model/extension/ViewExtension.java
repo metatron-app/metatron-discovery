0a0143ddd11ebd73796ec85a704e54100369351d
Kyungtaak Noh <kyungtaak@gmail.com>
2018-11-26 22:49:20 +0900

    #616 add view extension for selection query in WFS

--- discovery-server/src/main/java/app/metatron/discovery/domain/geo/query/model/extension/ViewExtension.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/geo/query/model/extension/ViewExtension.java
@@ -47,90 +47,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
-import java.util.Map;
 
-import app.metatron.discovery.common.GlobalObjectMapper;
 import app.metatron.discovery.query.druid.Aggregation;
 import app.metatron.discovery.query.druid.Dimension;
 import app.metatron.discovery.query.druid.Filter;
 import app.metatron.discovery.query.druid.PostAggregation;
 import app.metatron.discovery.query.druid.virtualcolumns.VirtualColumn;
 
-public class AggregationExtension implements DruidExtension {
+public class ViewExtension extends AggregationExtension implements DruidExtension {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AggregationExtension.class);
-
-  Filter filter;
-
-  List<VirtualColumn> virtualColumns;
-
-  List<Dimension> dimensions;
-
-  List<Aggregation> aggregators;
-
-  List<PostAggregation> postAggregators;
-
-  String boundary;
-
-  Map<String, Object> boundaryJoin;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ViewExtension.class);
 
   @JsonCreator
-  public AggregationExtension(
+  public ViewExtension(
       @JsonProperty("virtualColumns") List<VirtualColumn> virtualColumns,
       @JsonProperty("filter") Filter filter,
       @JsonProperty("dimensions") List<Dimension> dimensions,
       @JsonProperty("aggregators") List<Aggregation> aggregators,
-      @JsonProperty("postAggregators") List<PostAggregation> postAggregators,
-      @JsonProperty("boundary") String boundary,
-      @JsonProperty("boundaryJoin") Map<String, Object> boundaryJoin) {
-    this.virtualColumns = virtualColumns;
-    this.filter = filter;
-    this.dimensions = dimensions;
-    this.aggregators = aggregators;
-    this.postAggregators = postAggregators;
-    this.boundary = boundary;
-    this.boundaryJoin = boundaryJoin;
+      @JsonProperty("postAggregators") List<PostAggregation> postAggregators) {
+
+    super(virtualColumns, filter, dimensions, aggregators, postAggregators, null, null);
   }
 
-  public List<VirtualColumn> getVirtualColumns() {
-    return virtualColumns;
-  }
-
-  public Filter getFilter() {
-    return filter;
-  }
-
-  public List<Dimension> getDimensions() {
-    return dimensions;
-  }
-
-  public List<Aggregation> getAggregators() {
-    return aggregators;
-  }
-
-  public List<PostAggregation> getPostAggregators() {
-    return postAggregators;
-  }
-
-  public String getBoundary() {
-    return boundary;
-  }
-
-  public Map<String, Object> getBoundaryJoin() {
-    return boundaryJoin;
-  }
-
-  @Override
-  public String toParamString() {
-    String queryStr = GlobalObjectMapper.writeValueAsString(this);
-
-    LOGGER.debug("Aggregation Extension : " + queryStr);
-
-    queryStr = StringUtils.replace(queryStr, ",", "\\,");
-
-    return "druid:" + queryStr;
-  }
 }
