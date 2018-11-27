196de27f4a3c7654c678b705228316754a75108a
Kyungtaak Noh <kyungtaak@gmail.com>
2018-11-27 18:01:02 +0900

    #920 add "geohex" type for geo aggregation, and set default aggregation type

--- discovery-server/src/test/java/app/metatron/discovery/domain/datasource/data/DataQueryRestIntegrationTest.java
+++ discovery-server/src/test/java/app/metatron/discovery/domain/datasource/data/DataQueryRestIntegrationTest.java
@@ -17,6 +17,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specic language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -1887,7 +1901,7 @@ public class DataQueryRestIntegrationTest extends AbstractRestIntegrationTest {
         new ExpressionFilter("amt < 50000 && amt > 40000")
     );
 
-    GeoHashFormat hashFormat = new GeoHashFormat("h3", 5);
+    GeoHashFormat hashFormat = new GeoHashFormat("geohex", 5);
     DimensionField geoDimensionField = new DimensionField("gis", null, hashFormat);
 
     List<Field> layer1 = Lists.newArrayList(geoDimensionField,
