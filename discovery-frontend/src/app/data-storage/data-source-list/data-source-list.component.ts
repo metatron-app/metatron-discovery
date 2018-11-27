d09f28313045c23e0f6fc5f8b6fa2781da2ece41
Wonjune Choi <circlegiven@exntu.com>
2018-11-27 19:13:54 +0900

    #684 remove not used method in datasource list

--- discovery-frontend/src/app/data-storage/data-source-list/data-source-list.component.ts
+++ discovery-frontend/src/app/data-storage/data-source-list/data-source-list.component.ts
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { Component, ElementRef, Injector, OnInit, ViewChild } from '@angular/core';
 import { AbstractComponent } from '../../common/component/abstract.component';
 import { DatasourceService } from '../../datasource/service/datasource.service';
@@ -19,12 +18,9 @@ import { Datasource, SourceType, Status } from '../../domain/datasource/datasour
 import { Alert } from '../../common/util/alert.util';
 import { Modal } from '../../common/domain/modal';
 import { DeleteModalComponent } from '../../common/component/modal/delete/delete.component';
-import { PeriodComponent } from '../../common/component/period/period.component';
 import { MomentDatePipe } from '../../common/pipe/moment.date.pipe';
 import { CriterionKey, DatasourceCriterion } from '../../domain/datasource/datasourceCriterion';
 import { StringUtil } from '../../common/util/string.util';
-
-declare let moment: any;
 
 @Component({
   selector: 'app-data-source',
@@ -36,46 +32,27 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
   // criterion data object
   private _criterionDataObject: any = {};
 
-  // 선택한 데이터 타입 필터링
-  private searchDataType: string = 'all';
+  // origin criterion list
+  private _originCriterionList: DatasourceCriterion[] = [];
 
-
-  // 삭제할 소스 아이디
-  private deleteSourceId: string = '';
+  // origin more criterion more list
+  private _originMoreCriterionList: DatasourceCriterion[] = [];
 
   // 공통 삭제 팝업 모달
   @ViewChild(DeleteModalComponent)
   private deleteModalComponent: DeleteModalComponent;
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Protected Variables
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Public Variables
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
   public mode: string;
 
-  // 데이터 소스
-  public datasources: Datasource[];
+  // datasource list
+  public datasourceList: Datasource[];
 
   // search
   public searchKeyword: string = '';
 
-  // 정렬
-  public selectedContentSort: Order = new Order();
-
-
-  // TODO 684
-  private _originDatasourceFilterList: DatasourceCriterion[] = [];
+  // datasource filter list (for UI)
   public datasourceFilterList: DatasourceCriterion[] = [];
-  // origin more criterion more list
-  public _originMoreCriterionList: DatasourceCriterion[] = [];
-
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Constructor
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
   // 생성자
   constructor(private datasourceService: DatasourceService,
@@ -85,29 +62,25 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
     super(elementRef, injector);
   }
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Override Method
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
   // Init
   public ngOnInit() {
     // Init
     super.ngOnInit();
     // ui 초기화
-    this.initView();
+    this._initView();
     // loading show
     this.loadingShow();
     // get criterion list
     this.datasourceService.getDatasourceCriterionList()
       .then((result: DatasourceCriterion[]) => {
-        // set origin datasource filter list
-        this._originDatasourceFilterList = result;
+        // set origin criterion list
+        this._originCriterionList = result;
         // set datasource filter list
         this.datasourceFilterList = result;
         // set origin more criterion list
         this._originMoreCriterionList = result.find(criterion => criterion.criterionKey === CriterionKey.MORE).subCriteria;
-        // get datasource list
-        this.getDatasource();
+        // set datasource list
+        this._setDatasourceList();
       }).catch(reason => this.commonExceptionHandler(reason));
   }
 
@@ -125,46 +98,38 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
    */
   public createComplete(): void {
     this.changeMode( '' );
-    // 재조회
-    this.getDatasource();
+    // set datasource list
+    this._setDatasourceList();
   }
 
   /**
-   * 데이터소스 삭제
+   * Remove datasource
+   * @param {Modal} modalData
    */
-  public deleteSource(): void {
+  public removeDatasource(modalData: Modal): void {
     // 로딩 show
     this.loadingShow();
     // 데이터소스 삭제
-    this.datasourceService.deleteDatasource(this.deleteSourceId)
+    this.datasourceService.deleteDatasource(modalData.data)
       .then((result) => {
         Alert.success(this.translateService.instant('msg.storage.alert.dsource.del.success'));
-        // 초기화
-        this.deleteSourceId = '';
-        // 재조회
-        this.getDatasource();
+        // set datasource list
+        this._setDatasourceList();
       })
-      .catch((error) => {
-        this.commonExceptionHandler(error);
-      });
+      .catch(error => this.commonExceptionHandler(error));
   }
 
   /**
-   * 데이터 소스 삭제 모달 오픈
-   * @param source
+   * Remove datasource click event
+   * @param {Datasource} datasource
    */
-  public deleteModalOpen(source): void {
-    // event 전파 방지
-    event.stopPropagation();
-
+  public onClickRemoveDatasource(datasource: Datasource): void {
     const modal = new Modal();
     modal.name = this.translateService.instant('msg.storage.ui.dsource.del.title');
     modal.description = this.translateService.instant('msg.storage.ui.dsource.del.description');
     modal.btnName = this.translateService.instant('msg.storage.btn.dsource.del');
-
-    // 삭제할 소스 아이디 저장
-    this.deleteSourceId = source.id;
-
+    // set source id
+    modal.data = datasource.id;
     // 팝업 창 오픈
     this.deleteModalComponent.init(modal);
   }
@@ -180,30 +145,14 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
   /**
    * 더보기 버튼
    */
-  public getMoreList(): void {
+  public onClickMoreDatasourceList(): void {
     // 더 보여줄 데이터가 있다면
     if (this.isMoreContents()) {
-
+      // add page number
       this.page.page += 1;
-      // 데이터소스 조회
-      this.getDatasource();
+      // set datasource list
+      this._setDatasourceList();
     }
-  }
-
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Public Method - event
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
-  /**
-   * 필터링 타입 변경
-   * @param type
-   */
-  public onChangeType(type): void {
-    this.searchDataType = type.value;
-    // 페이지 초기화
-    this.page.page = 0;
-    // 재조회
-    this.getDatasource();
   }
 
   /**
@@ -236,38 +185,13 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
   }
 
   /**
-   * 필터링 데이터 소스 정렬
-   * @param {string} key
-   */
-  public sort(key: string): void {
-    // 정렬 정보 저장
-    this.selectedContentSort.key = key;
-
-    if (this.selectedContentSort.key === key) {
-      // asc, desc
-      switch (this.selectedContentSort.sort) {
-        case 'asc':
-          this.selectedContentSort.sort = 'desc';
-          break;
-        case 'desc':
-          this.selectedContentSort.sort = 'asc';
-          break;
-      }
-    }
-    // 페이지 초기화
-    this.page.page = 0;
-    // 재조회
-    this.getDatasource();
-  }
-
-  /**
    * Search datasource
    */
   public searchDatasource(): void {
     // 페이지 초기화
     this.page.page = 0;
-    // 데이터소스 리스트 조회
-    this.getDatasource();
+    // set datasource list
+    this._setDatasourceList();
   }
 
   /**
@@ -287,40 +211,30 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
     }
   }
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Public Method - getter
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
   /**
-   * 데이터 소스 status
-   * @param {Datasource} datasource
+   * Get datasource status label
+   * @param {Status} status
    * @returns {string}
    */
-  public getSourceStatus(datasource: Datasource): string {
-    const status = datasource.status;
-    let result = 'Disabled';
+  public getSourceStatusLabel(status: Status): string {
     switch (status) {
       case Status.ENABLED:
-        result = 'Enabled';
-        break;
+        return 'Enabled';
       case Status.PREPARING:
-        result = 'Preparing';
-        break;
+        return 'Preparing';
       case Status.DISABLED:
-        result = 'Disabled';
-        break;
+        return 'Disabled';
       case Status.FAILED:
-        result = 'Failed';
-        break;
+        return 'Failed';
+      case Status.BAD:
+        return 'Bad';
       default:
-        result = 'Disabled';
-        break;
+        return 'Disabled';
     }
-    return result;
   }
 
   /**
-   * 데이터 소스 status icon
+   * Get datasource status icon
    * @param {Status} status
    * @returns {string}
    */
@@ -360,7 +274,7 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
   }
 
   /**
-   * 더 조회할 컨텐츠가 있는지 여부
+   * Is more datasource list
    * @returns {boolean}
    */
   public isMoreContents(): boolean {
@@ -368,67 +282,47 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
   }
 
   /**
-   * 컨텐츠 총 갯수
-   * @returns {number}
+   * Set datasource list
+   * @private
    */
-  public get getTotalContentsCount(): number {
-    return this.pageResult.totalElements;
-  }
-
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Protected Method
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Private Method
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
-  /**
-   * 데이터 소스 목록조회
-   */
-  private getDatasource(): void {
-    // 로딩 show
+  private _setDatasourceList(): void {
+    // loading show
     this.loadingShow();
     // 데이터 소스 조회 요청
     this.datasourceService.getDatasourceList(this.page.page, this.page.size, this._getDatasourceParams())
-      .then((datasources) => {
-        // 페이지 객체
-        this.pageResult = datasources['page'];
-        // 페이지가 첫번째면
+      .then((result) => {
+        // set page result
+        this.pageResult = result['page'];
+        // if page number is 0
         if (this.page.page === 0) {
-          this.datasources = [];
+          // init datasource list
+          this.datasourceList = [];
         }
-        // 데이터 있다면
-        this.datasources = datasources['_embedded'] ? this.datasources.concat(datasources['_embedded'].datasources) : [];
-        // 로딩 hide
+        // set datasource list
+        this.datasourceList = result['_embedded'] ? this.datasourceList.concat(result['_embedded'].datasources) : [];
+        // loading hide
         this.loadingHide();
       })
-      .catch((error) => {
-        console.error(error);
-        // 로딩 hide
-        this.loadingHide();
-      });
+      .catch(error => this.commonExceptionHandler(error));
   }
 
   /**
-   * ui 초기화
+   * Init UI
+   * @private
    */
-  private initView(): void {
-    // 페이지 초기화
+  private _initView(): void {
+    // init page
     this.page.page = 0;
     this.page.size = 20;
   }
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Private Method - getter
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
-
   /**
-   * 데이터 소스 조회 파라메터
+   * Get datasource params
    * @returns {Object}
    * @private
    */
   private _getDatasourceParams(): object {
+    // params
     const params = {};
     // criterion filter list
     Object.keys(this._criterionDataObject).forEach((key) => {
@@ -448,16 +342,4 @@ export class DataSourceListComponent extends AbstractComponent implements OnInit
     }
     return params;
   }
-}
-
-class Order {
-  key: string = 'createdTime';
-  sort: string = 'desc';
-}
-
-class Date {
-  dateType: string;
-  endDateStr: string;
-  startDateStr: string;
-  type: string;
 }
