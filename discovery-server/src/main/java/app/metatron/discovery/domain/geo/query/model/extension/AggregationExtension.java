8f0f223928d57733cdaa6a2fbdd6c1fa1fcabecd
Kyungtaak Noh <kyungtaak@gmail.com>
2018-11-27 19:23:02 +0900

    #616 add logic to handle empty AndFilter

--- discovery-server/src/main/java/app/metatron/discovery/domain/geo/query/model/extension/AggregationExtension.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/geo/query/model/extension/AggregationExtension.java
@@ -40,11 +40,26 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specic language governing permissions and
+ * limitations under the License.
+ */
+
 package app.metatron.discovery.domain.geo.query.model.extension;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.util.StringUtils;
@@ -57,6 +72,7 @@ import app.metatron.discovery.query.druid.Aggregation;
 import app.metatron.discovery.query.druid.Dimension;
 import app.metatron.discovery.query.druid.Filter;
 import app.metatron.discovery.query.druid.PostAggregation;
+import app.metatron.discovery.query.druid.filters.AbstractCollectionFilter;
 import app.metatron.discovery.query.druid.virtualcolumns.VirtualColumn;
 
 public class AggregationExtension implements DruidExtension {
@@ -87,7 +103,16 @@ public class AggregationExtension implements DruidExtension {
       @JsonProperty("boundary") String boundary,
       @JsonProperty("boundaryJoin") Map<String, Object> boundaryJoin) {
     this.virtualColumns = virtualColumns;
-    this.filter = filter;
+
+    // Handling empty AndFilter
+    if (filter != null
+        && filter instanceof AbstractCollectionFilter
+        && CollectionUtils.isEmpty(((AbstractCollectionFilter) filter).getFields())) {
+      this.filter = null;
+    } else {
+      this.filter = filter;
+    }
+
     this.dimensions = dimensions;
     this.aggregators = aggregators;
     this.postAggregators = postAggregators;
