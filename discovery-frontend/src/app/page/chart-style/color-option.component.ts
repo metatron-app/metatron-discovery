439ef96023337ba036b2b8ae8fcc3a4f95a02609
juheeko <42233502+juheeko9@users.noreply.github.com>
2018-12-20 10:21:24 +0900

    #1069 change design displaying custom color design

--- discovery-frontend/src/app/page/chart-style/color-option.component.ts
+++ discovery-frontend/src/app/page/chart-style/color-option.component.ts
@@ -230,8 +230,10 @@ export class ColorOptionComponent extends BaseOptionComponent implements OnInit,
       }
     }
 
-    // set ranges for view
-    this.rangesViewList = this.setRangeViewByDecimal(this.uiOption.color['ranges']);
+    setTimeout(() => {
+      // set ranges for view
+      this.rangesViewList = this.setRangeViewByDecimal(this.uiOption.color['ranges']);
+    }, 100);
 
     // set min / max by decimal format
     if (this.uiOption.valueFormat && undefined !== this.uiOption.valueFormat.decimal) {
@@ -1218,6 +1220,54 @@ export class ColorOptionComponent extends BaseOptionComponent implements OnInit,
         return this.translateService.instant('msg.page.li.color.dimension');
       case ChartColorType.MEASURE.toString():
         return this.translateService.instant('msg.page.li.color.measure');
+    }
+  }
+
+  /**
+   * show / hide color min range span
+   */
+  public showMinInputColorRange(item, inputShow: boolean, minElement, index?: number) {
+
+    event.stopPropagation();
+
+    // hide other range preview
+    _.each(this.rangesViewList, (item) => {
+      if (item['minInputShow']) delete item['minInputShow'];
+      if (item['maxInputShow']) delete item['maxInputShow'];
+    });
+
+    item.minInputShow = inputShow;
+
+    if (undefined !== index) {
+
+      // show input box
+      this.changeDetect.detectChanges();
+      this.availableRange(item, index);
+      $(minElement).trigger('focus');
+    }
+  }
+
+  /**
+   * show / hide color max range span
+   */
+  public showMaxInputColorRange(item, inputShow: boolean, maxElement, index?: number) {
+
+    event.stopPropagation();
+
+    // hide other range preview
+    _.each(this.rangesViewList, (item) => {
+      if (item['minInputShow']) delete item['minInputShow'];
+      if (item['maxInputShow']) delete item['maxInputShow'];
+    });
+
+    item.maxInputShow = inputShow;
+
+    if (undefined !== index) {
+
+      // show input box
+      this.changeDetect.detectChanges();
+      this.availableRange(item, index);
+      $(maxElement).trigger('focus');
     }
   }
 
