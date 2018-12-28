2b848bdf055f0105e04cb536f9e14ecc4f97acf3
Wonjune Choi <circlegiven@exntu.com>
2018-12-28 13:50:41 +0900

    #1168 remove value validation in connection

--- discovery-frontend/src/app/data-storage/data-connection/handler-data-connection/create-data-connection/create-connection.component.ts
+++ discovery-frontend/src/app/data-storage/data-connection/handler-data-connection/create-data-connection/create-connection.component.ts
@@ -368,14 +368,15 @@ export class CreateConnectionComponent extends AbstractPopupComponent implements
       property.valueError = true;
       return;
     }
+    // #1168 - removed valid
     // check special characters, and korean (enable .dot)
-    if (property.value.trim().match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\{\}\[\]\/?,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi)) {
-      // set duplicate message
-      property.valueValidMessage = this.translateService.instant('msg.storage.ui.custom.property.special.char.disable');
-      // set error flag
-      property.valueError = true;
-      return;
-    }
+    // if (property.value.trim().match(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\{\}\[\]\/?,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi)) {
+    //   // set duplicate message
+    //   property.valueValidMessage = this.translateService.instant('msg.storage.ui.custom.property.special.char.disable');
+    //   // set error flag
+    //   property.valueError = true;
+    //   return;
+    // }
   }
 
   /**
