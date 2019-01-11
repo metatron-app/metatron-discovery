8f214dcb98da76957df72d0de15bea4c5a2e77cc
wonjune <circlegiven@exntu.com>
2019-01-11 18:19:08 +0900

    #1092 remove duration in detail source

--- discovery-frontend/src/app/data-storage/data-source-list/detail-data-source/information-dats-source/information-data-source.component.ts
+++ discovery-frontend/src/app/data-storage/data-source-list/detail-data-source/information-dats-source/information-data-source.component.ts
@@ -142,9 +142,6 @@ export class InformationDataSourceComponent extends AbstractPopupComponent imple
   // partitionKeyList
   public partitionKeyList: string[];
 
-  // is show ingestion interval
-  public isShowIngestionInterval: boolean;
-
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   | Constructor
   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
@@ -172,8 +169,6 @@ export class InformationDataSourceComponent extends AbstractPopupComponent imple
     if (!this.isLinkedSource() && this.isEnabled() && this.timestampColumn) {
       this._getFieldStats(this.timestampColumn.name, this.datasource.engineName);
     }
-    // set ingestion show flag
-    this.isShowIngestionInterval = !this.timestampColumn.derived && this.getIngestion.intervals && (!this.getSummary || (this.getSummary && this.getIngestion.intervals[0].split('/')[0] !== this.getSummary.ingestionMinTime && this.getIngestion.intervals[0].split('/')[1] !== this.getSummary.ingestionMaxTime));
   }
 
   /**
