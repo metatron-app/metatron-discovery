bb9f54d48e756f03be2f0217dfb504d574d67376
wonjune <circlegiven@exntu.com>
2019-01-25 16:38:23 +0900

    #1057 change file select step component

---
+++ discovery-frontend/src/app/data-storage/service/data-source-create.service.ts
@@ -0,0 +1,62 @@
+import {Injectable, Injector} from "@angular/core";
+import {FileItem, ParsedResponseHeaders} from "ng2-file-upload";
+import {TranslateService} from "ng2-translate";
+
+@Injectable()
+export class DataSourceCreateService {
+
+  private _translateService: TranslateService;
+
+  constructor(injector: Injector) {
+    this._translateService = injector.get(TranslateService);
+  }
+
+
+  public getConvertSheets(sheets: object): Sheet[] {
+    return Object.keys(sheets).map(key => {
+      return {sheetName: key, valid: sheets[key].valid, warning: sheets[key].warning};
+    });
+  }
+
+  /**
+   * Get file error message
+   * @param {string} errorCode
+   * @return {string}
+   */
+  public getFileErrorMessage(errorCode: string): string {
+    return this._translateService.instant(`msg.storage.ui.file.result.${errorCode}`);
+  }
+}
+
+export interface FileResult {
+  filePath: string;
+  fileKey: string;
+  fileSize: string;
+  fileName: string;
+  sheets?: Sheet[];
+  selectedSheet?: Sheet;
+}
+
+export interface Sheet {
+  sheetName: string;
+  valid: boolean;
+  warning?: string;
+}
+
+export interface UploadResult {
+  success: boolean;
+  item: FileItem;
+  response: any;
+  status: number;
+  headers: ParsedResponseHeaders;
+}
+
+export interface FileDetail {
+  data: any[];
+  fields: any[];
+  headers?: any[];
+  success?: boolean;
+  totalRows?: number;
+  isParsable: {valid: boolean, warning?: string};
+  errorMessage?: string
+}
