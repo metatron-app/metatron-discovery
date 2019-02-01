b3e4e13a0369e3cba7f09f82f1322787bed121fd
choong <choong@exntu.com>
2019-02-01 16:27:32 +0900

    f#1107 z-index un-proper working bugfix

--- discovery-frontend/proxy.conf.js
+++ discovery-frontend/proxy.conf.js
@@ -4,14 +4,14 @@ const PROXY_CONFIG = [
       "/api",
       "/oauth"
     ],
-    target: "http://52.231.184.135:8180",
+    target: "http://localhost:8180",
     secure: false
   },
   {
     context: [
       "/stomp"
     ],
-    target: "http://52.231.184.135:8180",
+    target: "http://localhost:8180",
     secure: false
   }
 ];
