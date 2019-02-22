8b0ed2856d2ae1fc6008de719793915ba5f605cd
choong <choong@exntu.com>
2019-02-22 17:16:16 +0900

    f#1107 ui clustering changed to server clustering update

--- discovery-frontend/src/app/datasource/service/datasource.service.ts
+++ discovery-frontend/src/app/datasource/service/datasource.service.ts
@@ -600,31 +600,31 @@ export class DatasourceService extends AbstractService {
                 }
 
                 // todo : cluster 서버 사용일 경우 변경
-                // // // clustering
-                // let chart = (<UIMapOption>pageConf.chart);
-                // if( chart.layers[idx].type == MapLayerType.SYMBOL && chart.layers[idx]['clustering'] ) {
-                //
-                //   // cluster 값 변경
-                //   let precision = Math.round((chart.layers[idx]['coverage']) / 8.33);
-                //   if (precision > 12) precision = 12;
-                //   if (precision < 1) precision = 1;
-                //
-                //   query.shelf.layers[idx].view = <GeoHashFormat>{
-                //     type: LayerViewType.CLUSTERING.toString(),
-                //     method: "h3",
-                //     // 0~99 퍼센트 값 변환
-                //     precision: precision
-                //   };
-                //
-                //   let spatialFilter = new SpatialFilter();
-                //   spatialFilter.dataSource = layer.ref;
-                //   spatialFilter.ref = layer.ref;
-                //   spatialFilter.field = layer.field.name;
-                //   // TODO : 최초 default 값 체크 (빈값일 경우 에러리턴)
-                //   spatialFilter.lowerCorner = _.isUndefined(chart['lowerCorner']) ? '-123.0998 25.4766' : chart['lowerCorner'];
-                //   spatialFilter.upperCorner = _.isUndefined(chart['upperCorner']) ? '-68.7918 48.7974' : chart['upperCorner'];
-                //   query.filters.push( spatialFilter );
-                // }
+                // // clustering
+                let chart = (<UIMapOption>pageConf.chart);
+                if( chart.layers[idx].type == MapLayerType.SYMBOL && chart.layers[idx]['clustering'] ) {
+
+                  // cluster 값 변경
+                  let clusterPrecision = Math.round((100 - chart.layers[idx]['coverage']) / 8.33);
+                  if (clusterPrecision > 12) clusterPrecision = 12;
+                  if (clusterPrecision < 1) clusterPrecision = 1;
+
+                  query.shelf.layers[idx].view = <GeoHashFormat>{
+                    type: LayerViewType.CLUSTERING.toString(),
+                    method: "h3",
+                    // 0~99 퍼센트 값 변환
+                    precision: clusterPrecision
+                  };
+
+                  let spatialFilter = new SpatialFilter();
+                  spatialFilter.dataSource = layer.ref;
+                  spatialFilter.ref = layer.ref;
+                  spatialFilter.field = layer.field.name;
+                  // TODO : 최초 default 값 체크 (빈값일 경우 에러리턴)
+                  spatialFilter.lowerCorner = _.isUndefined(chart['lowerCorner']) ? '-123.0998 25.4766' : chart['lowerCorner'];
+                  spatialFilter.upperCorner = _.isUndefined(chart['upperCorner']) ? '-68.7918 48.7974' : chart['upperCorner'];
+                  query.filters.push( spatialFilter );
+                }
               }
 
               // when logicalType => geo point
--- discovery-frontend/src/app/page/chart-style/map/map-layer-option.component.ts
+++ discovery-frontend/src/app/page/chart-style/map/map-layer-option.component.ts
@@ -337,9 +337,9 @@ export class MapLayerOptionComponent extends BaseOptionComponent {
 
   public changeClustering(obj: any, $event: any, index: number) {
     this.uiOption.layers[index]['coverage']= $event.from;
-    this.applyLayers();
+    // this.applyLayers();
     // todo : cluster 서버 사용일 경우 변경
-    // this.applyLayers({type : EventType.MAP_CHANGE_OPTION});
+    this.applyLayers({type : EventType.MAP_CHANGE_OPTION});
   }
 
   public changeClusteringText($event: any, index: number) {
