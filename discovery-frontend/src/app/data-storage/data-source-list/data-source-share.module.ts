cdd11099692ff0daf8f67d957b8be859b219fe0a
wonjune <circlegiven@exntu.com>
2019-02-26 14:27:23 +0900

    #1245 set service in source share module provider

--- discovery-frontend/src/app/data-storage/data-source-list/data-source-share.module.ts
+++ discovery-frontend/src/app/data-storage/data-source-list/data-source-share.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from "@angular/core";
 import {DataGridDataSourceComponent} from "./detail-data-source/data-grid-data-source/data-grid-data-source.component";
 import {CommonModule} from "../../common/common.module";
+import {DatasourceService} from "../../datasource/service/datasource.service";
+import {DataconnectionService} from "../../dataconnection/service/dataconnection.service";
+import {TimezoneService} from "../service/timezone.service";
 
 @NgModule({
   imports: [
@@ -11,6 +14,11 @@ import {CommonModule} from "../../common/common.module";
   ],
   exports: [
     DataGridDataSourceComponent,
+  ],
+  providers: [
+    DatasourceService,
+    DataconnectionService,
+    TimezoneService
   ]
 })
 export class DataSourceShareModule {}
