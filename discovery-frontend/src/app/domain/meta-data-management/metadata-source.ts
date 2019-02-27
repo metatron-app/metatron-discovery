70b761bfd2d959df53baa94428a5ed7eab9bd51b
ninezero90hy <ninezero90hy@gmail.com>
2019-02-27 14:56:34 +0900

    #1237 Modify Metadata Source Domains

--- discovery-frontend/src/app/domain/meta-data-management/metadata-source.ts
+++ discovery-frontend/src/app/domain/meta-data-management/metadata-source.ts
@@ -14,6 +14,7 @@
 
 import { AbstractHistoryEntity } from '../common/abstract-history-entity';
 import {Datasource} from '../datasource/datasource';
+import {Dataconnection} from '../dataconnection/dataconnection';
 
 export class MetadataSource extends AbstractHistoryEntity {
   // id
@@ -31,7 +32,7 @@ export class MetadataSource extends AbstractHistoryEntity {
   // metadata source detail
   public sourceInfo: string;
   // Datasource
-  public source: Datasource;
+  public source: Datasource | Dataconnection;
 }
 
 export enum MetadataSourceType {
