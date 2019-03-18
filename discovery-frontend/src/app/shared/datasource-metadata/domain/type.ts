70ca3297183aa614562ec5459af50a0a79d1f934
wonjune <circlegiven@exntu.com>
2019-03-18 13:05:52 +0900

    #1540 add Logical type

--- discovery-frontend/src/app/shared/datasource-metadata/domain/type.ts
+++ discovery-frontend/src/app/shared/datasource-metadata/domain/type.ts
@@ -14,6 +14,30 @@ export namespace Type {
   }
 
   export enum Logical {
-
+    STRING = 'STRING',
+    BOOLEAN = 'BOOLEAN',
+    NUMBER = 'NUMBER',       // Integer or Double
+    INTEGER = 'INTEGER',
+    DOUBLE = 'DOUBLE',
+    TIMESTAMP = 'TIMESTAMP',
+    LNG = 'LNG',
+    LNT = 'LNT',
+    GEO_POINT = 'GEO_POINT',          // [lat(latitude),lon(longitude)] structure for GEO
+    GEO_LINE = 'GEO_LINE',
+    GEO_POLYGON = 'GEO_POLYGON',
+    ARRAY = 'ARRAY',
+    STRUCT = 'STRUCT',
+    MAP_KEY = 'MAP_KEY',
+    MAP_VALUE = 'MAP_VALUE',
+    IP_V4 = 'IP_V4',         // IPv4 Address
+    DISTRICT = 'DISTRICT',      // District
+    EMAIL = 'EMAIL',
+    SEX = 'SEX',
+    CREDIT_CARD = 'CREDIT_CARD',   // Credit card
+    NIN = 'NIN',           // National Indentification Number (eq. SSN)
+    POSTAL_CODE = 'POSTAL_CODE',
+    PHONE_NUMBER = 'PHONE_NUMBER',  // Phone Number
+    URL = 'URL',
+    HTTP_CODE = 'HTTP_CODE'
   }
 }
