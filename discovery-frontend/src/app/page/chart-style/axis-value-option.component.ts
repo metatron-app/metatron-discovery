c9b69e2d5cd1efd9082b5d51bf412feadf14523e
sting009 <42328567+sting009@users.noreply.github.com>
2019-04-03 18:16:02 +0900

    #1791 Fix UI bugs in chart options
    * fn fix y axis setting range min value and tooltip label
    
    * fn fix global variable margin workbench and series name disable
    
    * fn fix max value validation and only min value set arbitrary max value setting
    
    * fn fix max value setting

--- discovery-frontend/src/app/page/chart-style/axis-value-option.component.ts
+++ discovery-frontend/src/app/page/chart-style/axis-value-option.component.ts
@@ -272,8 +272,11 @@ export class AxisValueOptionComponent extends FormatOptionComponent {
    */
   public changeMin(): void {
 
-    // 값이 비어있다면 0으로 치환
     let value = this.axisTemp.grid.min;
+    if( _.isNull(value) ) {
+      return;
+    }
+    // 값이 비어있다면 0으로 치환
     if( _.eq(value, "") ) {
       this.axisTemp.grid.min = 0;
     }
@@ -292,6 +295,14 @@ export class AxisValueOptionComponent extends FormatOptionComponent {
 
     let min: number = Number(this.axisTemp.grid.min);
     let max = Number(this.axis.grid.max);
+
+    // max 값이 0일 경우 (설정안했을경우)
+    if( max == 0 ){
+      let dataMax = AxisOptionConverter.axisMinMax[(_.eq(this.axis.mode,AxisLabelType.ROW) ? 'xAxis' : 'yAxis')].max;
+      max = Number(dataMax.toFixed(2));
+      this.axis.grid.max = max;
+    }
+
     if( min >= max ) {
       Alert.info(this.translateService.instant('msg.page.yaxis.grid.min.alert'));
       this.axisTemp.grid.min = this.axis.grid.min != 0 ? originAxisMin : null;
@@ -322,9 +333,14 @@ export class AxisValueOptionComponent extends FormatOptionComponent {
    */
   public changeMax(): void {
 
-    // 값이 비어있다면 0으로 치환
     let value = this.axisTemp.grid.max;
+    if( _.isNull(value) ) {
+      return;
+    }
+    // 값이 비어있다면 0으로 치환
+    let isDefaultValue : boolean = false;
     if( _.eq(value, "") ) {
+      isDefaultValue = true;
       this.axisTemp.grid.max = 0;
     }
 
@@ -342,14 +358,24 @@ export class AxisValueOptionComponent extends FormatOptionComponent {
 
     let min: number = Number(this.axis.grid.min);
     let max: number = !isNaN(this.axisTemp.grid.max) ? Number(this.axisTemp.grid.max) : 0;
-    if( max <= min ) {
+
+    // max 값이 0일 경우 (설정안했을경우)
+    if( isDefaultValue ){
+      let dataMax = AxisOptionConverter.axisMinMax[(_.eq(this.axis.mode,AxisLabelType.ROW) ? 'xAxis' : 'yAxis')].max;
+      max = Number(dataMax.toFixed(2));
+      this.axis.grid.max = max;
+    }
+
+    if( max <= min && (max != 0 && min != 0) || max == 0 ) {
       Alert.info(this.translateService.instant('msg.page.yaxis.grid.max.alert'));
-      this.axisTemp.grid.max = this.axis.grid.max != 0 ? originAxisMax : null;
+      this.axisTemp.grid.max = originAxisMax;
       return;
     }
 
     // 기준선 변경
-    this.axis.grid.max = Number(this.axisTemp.grid.max);
+    if (Number(this.axisTemp.grid.max) != 0)
+      this.axis.grid.max = Number(this.axisTemp.grid.max);
+
     if( this.axisTemp.grid.autoScaled || this.axis.grid.autoScaled ) {
       this.axisTemp.grid.autoScaled = false;
       this.axisTemp.grid.min = null;
