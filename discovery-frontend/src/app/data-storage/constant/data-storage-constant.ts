2b243fcb083514a5bbd4ac48f4be6b98105f487e
wonjune <circlegiven@exntu.com>
2019-05-09 18:26:38 +0900

    #1920 create data storage constant file

---
+++ discovery-frontend/src/app/data-storage/constant/data-storage-constant.ts
@@ -0,0 +1,17 @@
+export namespace DataStorageConstant {
+  export namespace Datasource {
+    export enum BroadcastKey {
+      DATASOURCE_CREATED_FIELD = 'DATASOURCE_CREATED_FIELD',
+      DATASOURCE_CHANGED_FIELD = 'DATASOURCE_CHANGED_FIELD',
+      DATASOURCE_CHANGED_FIELD_LIST = 'DATASOURCE_CHANGED_FIELD_LIST',
+      DATASOURCE_CHANGED_FIELD_LIST_FILTER = 'DATASOURCE_CHANGED_FIELD_LIST_FILTER',
+
+    }
+
+    export enum FilterKey {
+      ROLE = 'ROLE',
+      TYPE = 'TYPE',
+      SEARCH = 'SEARCH',
+    }
+  }
+}
