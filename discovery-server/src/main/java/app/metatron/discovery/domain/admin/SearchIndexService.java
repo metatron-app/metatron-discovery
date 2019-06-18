53f20b05997e0dc8b55c838b3b50ad4e1e2f231c
Kyungtaak Noh <kyungtaak@gmail.com>
2019-06-18 16:29:57 +0900

    #1704 fix error for java 11/12 compatibility

--- discovery-server/src/main/java/app/metatron/discovery/domain/admin/SearchIndexService.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/admin/SearchIndexService.java
@@ -20,13 +20,14 @@ import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.Search;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
 
 import app.metatron.discovery.domain.datasource.DataSource;
 import app.metatron.discovery.domain.user.User;
@@ -38,31 +39,35 @@ import app.metatron.discovery.domain.user.role.Role;
 @Component
 public class SearchIndexService {
 
-  private static Logger LOGGER = LoggerFactory.getLogger(SearchIndexService.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SearchIndexService.class);
 
-  public static Integer DEFAULT_BATCH_INDEX_SIZE = 25;
+  private final Integer DEFAULT_BATCH_INDEX_SIZE = 25;
 
-  public static List<Class<?>> INDEX_DOMAIN_CLASSES = Lists.newArrayList(DataSource.class, User.class, Role.class);
+  private final List<Class<?>> INDEX_DOMAIN_CLASSES = Lists.newArrayList(DataSource.class, User.class, Role.class);
 
-  @PersistenceContext
-  private EntityManager entityManager;
+  private final EntityManager entityManager;
+
+  @Autowired
+  public SearchIndexService(final EntityManagerFactory entityManagerFactory) {
+    this.entityManager = entityManagerFactory.createEntityManager();
+  }
 
   @Async
   public void reindexAll(int batchSize) {
     LOGGER.info("Start creating search index.");
 
-    if(batchSize <= 0) {
+    if (batchSize <= 0) {
       batchSize = DEFAULT_BATCH_INDEX_SIZE;
     }
 
     try {
       FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
       fullTextEntityManager.createIndexer(INDEX_DOMAIN_CLASSES.toArray(new Class[0]))
-          .typesToIndexInParallel(INDEX_DOMAIN_CLASSES.size())
-          .batchSizeToLoadObjects(batchSize)
-          .threadsToLoadObjects(5)
-          .idFetchSize(150)
-          .startAndWait();
+                           .typesToIndexInParallel(INDEX_DOMAIN_CLASSES.size())
+                           .batchSizeToLoadObjects(batchSize)
+                           .threadsToLoadObjects(5)
+                           .idFetchSize(150)
+                           .startAndWait();
       LOGGER.info("Successfully created search index.");
     } catch (InterruptedException e) {
       LOGGER.warn("An error occurred trying to build the search index: {}", e.toString());
