be48471f1def894c28a807451387109aa34a4e65
minhyun2 <jmhh8535@gmail.com>
2019-07-02 18:52:02 +0900

    #2303 fix default dataconnectino icon of extensions

--- discovery-frontend/src/app/workspace/workspace.component.ts
+++ discovery-frontend/src/app/workspace/workspace.component.ts
@@ -15,7 +15,12 @@
 import {Component, ElementRef, Injector, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AbstractComponent} from '../common/component/abstract.component';
 import {CreateWorkbookComponent} from '../workbook/component/create-workbook/create-workbook.component';
-import {CountByBookType, PermissionChecker, PublicType, Workspace} from '../domain/workspace/workspace';
+import {
+  CountByBookType,
+  PermissionChecker,
+  PublicType,
+  Workspace
+} from '../domain/workspace/workspace';
 import {WorkspaceService} from './service/workspace.service';
 import {ActivatedRoute} from '@angular/router';
 import {Book} from '../domain/workspace/book';
@@ -300,9 +305,9 @@ export class WorkspaceComponent extends AbstractComponent implements OnInit, OnD
       this._connTypeList = StorageService.connectionTypeList;
     }
     const connType = this.getConnType(book);
-    return this.getConnImplementorGrayImgUrl(
-      connType,
-      this._connTypeList.find(item => item.implementor === connType).iconResource3
+    return this.getConnImplementorGrayImgUrl(connType,
+      isNullOrUndefined(this._connTypeList.find(item => item.implementor === connType)) ?
+        null : this._connTypeList.find(item => item.implementor === connType).iconResource3
     );
   } // function - getWorkbenchConnTypeIcon
 
