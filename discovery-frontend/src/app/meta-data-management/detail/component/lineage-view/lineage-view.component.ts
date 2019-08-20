0a04935ead8f314256e3265c9479dfebe6068428
ksparknot <kay.park@sk.com>
2019-08-20 16:54:30 +0900

    #2471 disabled zoom of diagram

--- discovery-frontend/src/app/meta-data-management/detail/component/lineage-view/lineage-view.component.ts
+++ discovery-frontend/src/app/meta-data-management/detail/component/lineage-view/lineage-view.component.ts
@@ -394,11 +394,6 @@ export class LineageViewComponent extends AbstractComponent implements OnInit, O
 
     this.chartOptions = {
       backgroundColor: '#ffffff',
-      dataZoom: [
-        {
-          type: 'inside'
-        }
-      ],
       tooltip: { show: true },
       toolbox: {
         left: 'left',
