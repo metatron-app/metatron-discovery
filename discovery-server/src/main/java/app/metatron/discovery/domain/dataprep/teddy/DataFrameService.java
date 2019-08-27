4a9a5bfe7f1862eb56c94e1fbc51036c35dbc400
Kim, Joo-Ho <teamsprint@gmail.com>
2019-08-27 12:39:29 +0900

    #2457 fix teddy test cases

--- discovery-server/src/main/java/app/metatron/discovery/domain/dataprep/teddy/DataFrameService.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/dataprep/teddy/DataFrameService.java
@@ -88,12 +88,9 @@ public class DataFrameService {
     return applyRule(df, ruleString, null);
   }
 
-  public DataFrame applyRule(DataFrame df, String ruleString, List<DataFrame> slaveDfs) throws TeddyException {
+  public DataFrame applyRuleInternal(DataFrame df, String ruleString, List<DataFrame> slaveDfs, Integer cores, Integer timeout, Integer limitRows) throws TeddyException {
     LOGGER.trace("applyRule(): start");
 
-    Integer cores = prepProperties.getSamplingCores();
-    Integer timeout = prepProperties.getSamplingTimeout();
-    Integer limitRows = prepProperties.getSamplingLimitRows();
 
     List<Future<List<Row>>> futures = new ArrayList<>();
     Rule rule = new RuleVisitorParser().parse(ruleString);
@@ -153,6 +150,14 @@ public class DataFrameService {
 
     LOGGER.trace("applyRule(): end (parallel)");
     return newDf;
+  }
+
+  public DataFrame applyRule(DataFrame df, String ruleString, List<DataFrame> slaveDfs) throws TeddyException {
+    Integer cores = prepProperties.getSamplingCores();
+    Integer timeout = prepProperties.getSamplingTimeout();
+    Integer limitRows = prepProperties.getSamplingLimitRows();
+
+    return applyRuleInternal(df, ruleString, slaveDfs, cores, timeout, limitRows);
   }
 
   @Async("prepThreadPoolTaskExecutor")
