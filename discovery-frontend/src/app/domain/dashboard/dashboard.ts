4433ec1c81411bbc579f8160da1b0e4ae6bb08b4
i1befree <i1befree@gmail.com>
2020-09-07 15:20:31 +0900

    #3361 Fix bug for Association & Join edit view with linked datasource.

--- discovery-frontend/src/app/domain/dashboard/dashboard.ts
+++ discovery-frontend/src/app/domain/dashboard/dashboard.ts
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { AbstractHistoryEntity } from '../common/abstract-history-entity';
 import { Datasource, Field, FieldNameAlias, FieldValueAlias } from '../datasource/datasource';
 import { Widget } from './widget/widget';
