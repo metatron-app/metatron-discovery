40bfe0800a150ed149fcbfff430ba1afe092177a
minhyun2 <45194478+minhyun2@users.noreply.github.com>
2021-01-14 17:01:53 +0900

    #3553 fix private workspace name

--- discovery-frontend/src/app/workspace/workspace.component.ts
+++ discovery-frontend/src/app/workspace/workspace.component.ts
@@ -1553,7 +1553,7 @@ export class WorkspaceComponent extends AbstractComponent implements OnInit, OnD
         // 워크스페이스 데이터
         this.workspace = workspace;
         // 워크스페이스 이름
-        this.workspaceName = (workspace.publicType === PublicType.PRIVATE && workspace.createdBy && workspace.createdBy.fullName)? workspace.createdBy.fullName + '\'s Workspace': workspace.name;
+        this.workspaceName = (workspace.publicType === PublicType.PRIVATE && workspace.owner && workspace.owner.fullName)? workspace.owner.fullName + '\'s Workspace': workspace.name;
         // 워크스페이스 설명
         this.workspaceDescription = workspace.description;
         // 워크스페이스 소유자
