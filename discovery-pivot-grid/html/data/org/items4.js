6a8f3244e9e2f599627ead6d70568d3f80333524
Harry <freestrokes.hk@gmail.com>
2021-03-08 16:40:43 +0900

    #3604 Removed pivot grid in discovery

--- discovery-pivot-grid/html/data/org/items4.js
+++
@@ -1,7 +0,0 @@
-var items4 = [{
-    "SUM(Sales)": 334.0,
-    "Category": "Technology",
-    "Sub-Category": "Phones",
-    "SUM(Profit)": 25.0,
-    "timestamp": "2011-11-01T00:00:00.000Z"
-}];
--- discovery-pivot-grid/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-
-const common = require('./pivot.common');
-const view = require('./pivot.view');
-
-const zs = common()
-view(zs);
-
-module.exports = zs;
