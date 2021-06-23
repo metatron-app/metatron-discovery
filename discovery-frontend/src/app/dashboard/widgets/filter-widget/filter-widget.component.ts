f425684cd2bb2da786c8005381e394f281e9a17b
Gyojun.Goo <eltriny@exntu.com>
2021-06-23 16:14:44 +0900

    #3717 Fix search bar style error in filter widget

--- discovery-frontend/src/app/dashboard/widgets/filter-widget/filter-widget.component.ts
+++ discovery-frontend/src/app/dashboard/widgets/filter-widget/filter-widget.component.ts
@@ -64,7 +64,8 @@ declare let moment;
 
 @Component({
   selector: 'filter-widget',
-  templateUrl: './filter-widget.component.html'
+  templateUrl: './filter-widget.component.html',
+  styleUrls: ['./filter-widget.component.css']
 })
 export class FilterWidgetComponent extends AbstractWidgetComponent<FilterWidget> implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
@@ -499,7 +500,6 @@ export class FilterWidgetComponent extends AbstractWidgetComponent<FilterWidget>
             candidate.count = resultCandidate.count;
             candidate.name = resultCandidate.field;
             candidate.isTemporary = true;
-
             this.candidateList.unshift(candidate);
           }
         });
