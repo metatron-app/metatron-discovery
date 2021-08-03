71db6c127656c1c6e0c18e7248be2c86c2cbe958
Eugene <71209206+yovahu@users.noreply.github.com>
2021-08-03 14:43:16 +0800

    #3822 Russian language pack (#3826)
    * russian_localization
    
    * set xms previous

--- discovery-server/src/main/java/app/metatron/discovery/config/ApiResourceConfig.java
+++ discovery-server/src/main/java/app/metatron/discovery/config/ApiResourceConfig.java
@@ -284,6 +284,7 @@ public class ApiResourceConfig extends WebMvcConfigurerAdapter {
     registry.addViewController("/app/v2/assets/i18n/en.json").setViewName("forward:/resource/assets/i18n/en.json");
     registry.addViewController("/app/v2/assets/i18n/ko.json").setViewName("forward:/resource/assets/i18n/ko.json");
     registry.addViewController("/app/v2/assets/i18n/zh.json").setViewName("forward:/resource/assets/i18n/zh.json");
+    registry.addViewController("/app/v2/assets/i18n/ru.json").setViewName("forward:/resource/assets/i18n/ru.json");
     //        registry.addViewController("/app/v2/assets/images/img_photo.png").setViewName("forward:/resource/assets/images/img_photo.png");
     //        registry.addViewController("/assets/**").setViewName("forward:/resource/index.html");
     registry.addViewController("/app/**").setViewName("forward:/resource/index.html");
