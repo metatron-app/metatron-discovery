e653dcb156ff055e6673588e0e11dbb1d77a156d
Gyojun Goo <eltriny@naver.com>
2022-02-03 13:25:04 +0900

    #3944 frontend jest test (#3992)

--- discovery-frontend/README.md
+++ discovery-frontend/README.md
@@ -15,6 +15,14 @@ Node Modules 인스톨 (UI 프로젝트 루트로 이동 후)
 => npm install
 ```
 
+[테스트]
+
+테스트 파일 src > _ _ test _ _ 에 테스트 파일들을 넣어준 후 아래의 명령어를 실행합니다. 
+
+```
+test run => npm run test
+```
+
 [배포]
 
 Angular CLI 빌드 && Run
---
+++ discovery-frontend/src/__tests__/simple-dashboard-test.js
@@ -0,0 +1,77 @@
+const puppeteer = require('puppeteer');
+const {toMatchImageSnapshot} = require('jest-image-snapshot');
+
+function setSampleConfig(filename){
+  return {
+    failureThreshold:'1', // 허용 오차
+    failureThresholdType:'pixel',
+    customSnapshotIdentifier: filename
+  }
+}
+
+expect.extend({toMatchImageSnapshot})
+
+describe('simple dashboard testing', () =>{
+
+  let cookies;
+  let browser;
+
+  beforeAll(async () => {
+
+    // console.log( '>>>>>>>> beforeAll' );
+
+    browser = await puppeteer.launch({
+      headless:false
+    });
+
+    const loginPage = await browser.newPage();
+    await loginPage.goto('http://t1-discovery.metatron.bundang10f.io/app/v2/user/login');
+
+    // login
+    const adminId = 'admin';
+    const adminPwd = 'admin';
+
+    await loginPage.type('#loginId', adminId);
+    await loginPage.type('#loginPwd', adminPwd);
+
+    await loginPage.click('.ddp-btn'); // login button
+    await loginPage.waitForResponse(response => {
+      // console.log( '>>>>>>> response url - ' + response.url() + ' / status - ' + response.status() );
+      return response.status() === 200 && response.url().includes('/api/workspaces/my');
+    });
+
+    // console.log( '>>>>>>>> before get cookie');
+
+    // Get cookies
+    cookies = await loginPage.cookies('http://t1-discovery.metatron.bundang10f.io');
+
+    // console.log( '>>>>>>>> after get cookie - ' + JSON.stringify( cookies ) );
+
+    await loginPage.close();
+
+  });
+
+  it('Load grid dashboard', async () => {
+
+    // console.log('==== START : Load grid dashboard test');
+
+    // Use cookies in other tab or browser
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.setCookie(...cookies);
+    await page.goto('http://t1-discovery.metatron.bundang10f.io/app/v2/workbook/1964be68-a5a0-4cb5-a8b7-bc4579b5b90d');
+    await page.waitForSelector('.ddp-box-widget .pivot-view > .pivot-view-body > .pivot-view-wrap');
+
+    // 화면 캡쳐 및 비교
+    const testImage = await page.screenshot({fullPage: true});
+    expect(testImage).toMatchImageSnapshot(setSampleConfig('simple-board-test'));
+    // console.log('==== END : Load grid dashboard test');
+
+    await page.close();
+  });
+
+  afterAll(async () => {
+    // console.log( '>>>>>>>> afterAll' );
+    await browser.close();
+  });
+});
