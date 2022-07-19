9a2c7bc2a31f5b7b539bddfbd351b5bb93ff588c
eltriny <12345!@>
2022-07-19 23:12:33 +0900

    #4048 Fixed an error when there were multiple points

--- discovery-frontend/src/app/datasource/service/datasource.service.ts
+++ discovery-frontend/src/app/datasource/service/datasource.service.ts
@@ -799,8 +799,8 @@ export class DatasourceService extends AbstractService {
                 if (geoFieldArr[idx] > 1) {
                   layer.format = ({
                     type: FormatType.GEO_BOUNDARY.toString(),
-                    geoColumn: query.pivot.columns[0].field.name,
-                    descColumn: query.pivot.columns[0].field.name
+                    geoColumn: query.shelf.layers[idx].fields[0].name,
+                    descColumn: query.shelf.layers[idx].fields[0].name
                   } as GeoBoundaryFormat)
                 }
 
